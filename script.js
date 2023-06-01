13e08c2da11392bf53bf30ebafb55d6aa5f690b2
Darshan <darshankdarsh01@gmail.com>
2023-06-01 21:07:18 +0530

    animate sorting process

---
+++ script.js
@@ -0,0 +1,52 @@
+const n=10;
+const array=[];
+function init(){
+    for(let i=0;i<10;i++){
+        array[i]=Math.random();
+    }
+    console.clear();
+    console.log(array);
+    
+    showBars();
+}
+function showBars(){
+    container.innerHTML="";
+    for(let i=0;i<array.length;i++){
+        const bar=document.createElement("div");
+        bar.style.height=array[i]*100+"%";
+        bar.classList.add("bar");
+        container.appendChild(bar);
+    }
+}
+function play(){
+    const copy=[...array];
+    swaps=bubbleSort(copy);
+    //showBars();
+    animate(swaps);
+}
+function animate(swaps){
+    if(swaps.length==0)
+        return;
+    const [i,j]=swaps.shift();
+    [array[i],array[j]]=[array[j],array[i]];
+    showBars();
+    setTimeout(function(){
+        animate(swaps);
+    },50);
+}
+
+function bubbleSort(array){
+    //sorting Algorithm
+    swaps=[];
+do{
+    var swapped=false;
+    for(let i=1;i<array.length;i++){
+       if(array[i-1]>array[i]){
+        swapped=true;
+        [array[i-1],array[i]]=[array[i],array[i-1]]
+        swaps.push([i-1,i]);
+       }
+    }
+}while(swapped);
+return swaps;
+}
